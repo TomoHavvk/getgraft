6ab2d42b935427d325c93a8a2a41d7de851ecbe9
Ihor Zadyra <izadyra@evolutiongaming.com>
2019-04-19 21:27:08 +0300

    Add simple metrics

--- src/main/scala/com/tomohavvk/snwatcher/http/server/IndexHttp.scala
+++ src/main/scala/com/tomohavvk/snwatcher/http/server/IndexHttp.scala
@@ -8,22 +8,43 @@ import akka.http.scaladsl.server.{Directive0, Route, _}
 import akka.stream.ActorMaterializer
 import com.tomohavvk.snwatcher.service.Services
 import com.tomohavvk.snwatcher.util.JsonUtil
+import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.ExecutionContextExecutor
 
-case class IndexHttp(services: Services)(implicit val system: ActorSystem, materializer: ActorMaterializer) extends HasRoute with Directives {
+case class IndexHttp(services: Services)(implicit val system: ActorSystem, materializer: ActorMaterializer) extends HasRoute with Directives with LazyLogging {
   implicit val ec: ExecutionContextExecutor = system.dispatcher
+  val ips = scala.collection.mutable.SortedSet.empty[String]
+  var indexCounter = 0
+  var snCounter = 0
 
   override def route: Route =
     corsHandler(
       handleRejections(rejectionHandler) {
-        get {
-          path("") {
-            getFromResource("webapp/dist/index.html")
-          } ~ path("sn") {
-            complete(services.nodes.map(nodes => JsonUtil.toJson(nodes)))
-          } ~ {
-            getFromResourceDirectory("webapp/dist/")
+        extractClientIP { IP =>
+          val ip = IP.toOption.map(_.getHostAddress).getOrElse("unknown")
+
+          get {
+            path("") {
+              indexCounter += 1
+              ips += ip
+              logger.info("index endpoint: " + ip)
+              getFromResource("webapp/dist/index.html")
+            } ~ path("sn") {
+              logger.info("sn endpoint: " + ip)
+              snCounter += 1
+              complete(services.nodes.map(nodes => JsonUtil.toJson(nodes)))
+            } ~ path("visitor") {
+              complete(JsonUtil.toJson(ips))
+            }~ path("visitor-size") {
+              complete(ips.size.toString)
+            }~ path("indexCounter") {
+              complete(indexCounter.toString)
+            }  ~ path("snCounter") {
+              complete(snCounter.toString)
+            } ~ {
+              getFromResourceDirectory("webapp/dist/")
+            }
           }
         }
       })
