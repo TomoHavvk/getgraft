9a7df6805d3244e7acf6167f2f7ede8b85c5fa1a
Ihor Zadyra <izadyra@evolutiongaming.com>
2019-04-26 14:37:36 +0300

    Add https supporting

--- src/main/scala/com/tomohavvk/snexplorer/http/server/HttpServer.scala
+++ src/main/scala/com/tomohavvk/snexplorer/http/server/HttpServer.scala
@@ -1,16 +1,19 @@
 package com.tomohavvk.snexplorer.http.server
 
+import java.io.InputStream
+import java.security.{KeyStore, SecureRandom}
 import java.time.Instant
 
 import akka.Done
 import akka.actor.ActorSystem
 import akka.event.Logging
-import akka.http.scaladsl.Http
+import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -35,7 +38,8 @@ class HttpServer(routes: Seq[HasRoute])(
   val serverBinding: Future[Http.ServerBinding] = {
     logger.info(s"Starting AkkaHttp server on port $port")
 
-    Http().bindAndHandle(route, interface = interface, port = port)
+    Http().bindAndHandle(route, interface, port = 443, connectionContext = https)
+//    Http().bindAndHandle(route, interface = interface, port = port, connectionContext = https)
   }
 
   def shutdown(hardDeadline: FiniteDuration): Future[Done] = {
@@ -43,12 +47,34 @@ class HttpServer(routes: Seq[HasRoute])(
 
     serverBinding flatMap (_ terminate hardDeadline) map (_ => Done)
   }
+
+
+  def https: HttpsConnectionContext = {
+    val password: Array[Char] = sslPassword.toCharArray // do not store passwords in code, read them from somewhere safe!
+
+    val ks: KeyStore = KeyStore.getInstance("PKCS12")
+    val keystore: InputStream = getClass.getClassLoader.getResourceAsStream("getgraft.p12")
+
+    require(keystore != null, "Keystore required!")
+    ks.load(keystore, password)
+
+    val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(ks, password)
+
+    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+    tmf.init(ks)
+
+    val sslContext: SSLContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
+    ConnectionContext.https(sslContext)
+  }
 }
 
 object HttpServer {
   private val config = ConfigFactory.load() getConfig "snexplorer.http.server"
   private val interface = config.getString("interface")
   private val port = config.getInt("port")
+  private val sslPassword = config.getString("ssl-password")
 
   def apply(routes: HasRoute*)(implicit system: ActorSystem, materializer: Materializer): HttpServer = {
     new HttpServer(Seq(routes: _*))
